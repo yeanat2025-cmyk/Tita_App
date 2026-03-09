b3cc791d9a57c630adccc743c5ab94bb23d58195
yeanat2025-cmyk <yeanat2025@gmail.com>
2026-03-09 22:06:28 +0600

    Add files via upload

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAavkx6aU0hiAzdcP70wjdG-YEXYmm1mdc",
+  authDomain: "game24-app.firebaseapp.com",
+  databaseURL: "https://game24-app-default-rtdb.firebaseio.com",
+  projectId: "game24-app",
+  storageBucket: "game24-app.firebasestorage.app",
+  messagingSenderId: "551706234613",
+  appId: "1:551706234613:web:4fd71e34e07b8b7a4dbfbc",
+  measurementId: "G-LJ0RZLS9MB"
+};
